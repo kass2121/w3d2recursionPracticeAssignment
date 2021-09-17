477c4bd60de8462cedc1c57240428a8876f87dd7
Kassaw  Ayalew <79111500+kass2121@users.noreply.github.com>
2021-09-17 01:15:18 -0400

    Update spread.js

--- spread.js
+++ spread.js
@@ -5,17 +5,50 @@
 //module.exports = {copyArray, concat, findMin, combineObjs }; //add all of your function names here that you need for the node mocha tests
 
 
-
+/**
+ * 
+ * @param {Array} arr any array input
+ * @returns {Array} newArr returns new array result
+ */
 function copyArray(arr){
-
+    let newArr = [...arr];
+    return newArr;
 }
+
+/**
+ * 
+ * @param {Array} arr1 any input array
+ * @param {Array} arr2 any input array
+ * @returns {Array} result concated array
+ */
 
 function concat(arr1, arr2){
-
+    let result = arr1.concat(arr2);
+    return result;
 }
 
+/**
+ * 
+ * @param  {...any} numbers any input numbers 
+ * @returns {number} min returns minimum value from the input numbers 
+ */
 function findMin(...numbers) {
-
+    let min = Infinity;
+    for (let i = 0; i < numbers.length; i++) {
+        if (arguments[i] < min) {
+            min = arguments[i];
+        }
+    }
+    return min;
 }
 
+/**
+ * 
+ * @param {Object} obj1 any new input object 
+ * @param {Object} obj2 any new input object 
+ * @returns {return} obj returns new combined object
+ */
 function combineObjs(obj1, obj2){
+    let obj = {...obj1,...obj2};
+    return obj;
+}
